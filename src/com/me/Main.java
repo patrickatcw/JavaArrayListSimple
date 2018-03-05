34f1691d418199dd9daac94513f0e3470140503b
patrickatcw <pj8392@yahoo.com>
2018-03-05 18:33:34 -0500

    step 1 declaring a arraylist variable

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,8 +1,20 @@
 package com.me;
 
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //step 1
+        //declare an ArrayList variable and call the ArrayList constructor to instantiate an ArrayList object and
+        // assign it to the variable:
+        //type in 6 for a specific list capacity
+        ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>(6);
+
+
+
 
     }
 
